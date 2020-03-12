d61ad05cbec1f1abb0a1f078b1d0394bd72a0f3a
heteddy <heteddy@126.com>
2020-03-13 01:13:43 +0800

    basic function

--- tidb/mergesort/mergesort_test.go
+++ tidb/mergesort/mergesort_test.go
@@ -18,7 +18,7 @@ func TestT(t *testing.T) {
 func prepare(src []int64) {
 	rand.Seed(time.Now().Unix())
 	for i := range src {
-		src[i] = rand.Int63()
+		src[i] = rand.Int63n(9999)
 	}
 }
 
@@ -26,6 +26,7 @@ type sortTestSuite struct{}
 
 func (s *sortTestSuite) TestMergeSort(c *check.C) {
 	lens := []int{1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 1024, 1 << 13, 1 << 17, 1 << 19, 1 << 20}
+	//lens := []int{11, 13, 17, 19, 23, 29, 100}
 
 	for i := range lens {
 		src := make([]int64, lens[i])
@@ -34,8 +35,11 @@ func (s *sortTestSuite) TestMergeSort(c *check.C) {
 		copy(expect, src)
 		MergeSort(src)
 		sort.Slice(expect, func(i, j int) bool { return expect[i] < expect[j] })
+		//log.Println("   src:",src)
+		//log.Println("expect:",expect)
 		for i := 0; i < len(src); i++ {
 			c.Assert(src[i], check.Equals, expect[i])
 		}
+
 	}
 }
